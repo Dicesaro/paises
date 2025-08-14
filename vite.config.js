8eff62ced491ff088dff8c1ccaf180847fbda81c
Cesar <cesarluis4056@gmail.com>
2025-08-14 17:13:14 -0500

    add icon

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //   base: 'https://Dicesaro.github.io/paises/',
+  base: 'https://Dicesaro.github.io/paises/',
 })
